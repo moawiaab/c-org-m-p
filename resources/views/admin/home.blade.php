f0629a6d157a30346713f52bfdf286b9362cdeaf
moawia-ab <moawiaab@gmail.com>
2022-02-07 10:42:47 +0200

    add dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,26 +1,374 @@
 @extends('layouts.admin')
 @push('styles')
-    <link rel="stylesheet" href="{{asset('admin-let/plugins/bootstrap-switch/css/bootstrap3/bootstrap-switch.min.css')}}">
+<link rel="stylesheet" href="{{asset('admin-let/plugins/bootstrap-switch/css/bootstrap3/bootstrap-switch.min.css')}}">
 @endpush
 @section('content')
 
+<section class="content">
+  <div class="container-fluid">
+    <!-- Small boxes (Stat box) -->
+    <div class="row">
+      <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-info">
+          <div class="inner">
+            <h3>{{ $data['budget']}}</h3>
 
-<div class="card card-secondary">
-    <div class="card-header">
-      <h3 class="card-title">Bootstrap Switch</h3>
+            <p>Budget</p>
+          </div>
+          <div class="icon">
+            <i class="ion ion-bag"></i>
+          </div>
+          <a href="{{ route("admin.budgets.index") }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+      <!-- ./col -->
+      <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-success">
+          <div class="inner">
+            <h3>{{ $data['donor']}}<sup style="font-size: 20px"></sup></h3>
+
+            <p>Donors</p>
+          </div>
+          <div class="icon">
+            <i class="ion ion-stats-bars"></i>
+          </div>
+          <a href="{{ route("admin.donors.index") }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+      <!-- ./col -->
+      <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-warning">
+          <div class="inner">
+            <h3>{{$data['project']}}</h3>
+
+            <p>Projects</p>
+          </div>
+          <div class="icon">
+            <i class="ion ion-person-add"></i>
+          </div>
+          <a href="{{ route("admin.projects.index") }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+      <!-- ./col -->
+      <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-danger">
+          <div class="inner">
+            <h3>{{$data['expense']}}</h3>
+
+            <p>Expense</p>
+          </div>
+          <div class="icon">
+            <i class="ion ion-pie-graph"></i>
+          </div>
+          <a href="{{ route("admin.expenses.index") }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+      <!-- ./col -->
     </div>
-    <div class="card-body">
-      <input type="checkbox" name="my-checkbox" checked data-bootstrap-switch>
-      <input type="checkbox" name="my-checkbox" checked data-bootstrap-switch data-off-color="danger" data-on-color="success">
+    <!-- /.row -->
+    <!-- Main row -->
+    <div class="row">
+      <!-- Left col -->
+      <section class="col-lg-7 connectedSortable">
+        <!-- Custom tabs (Charts with tabs)-->
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">
+              <i class="fas fa-chart-pie mr-1"></i>
+              Sales
+            </h3>
+            <div class="card-tools">
+              <ul class="nav nav-pills ml-auto">
+                <li class="nav-item">
+                  <a class="nav-link active" href="#revenue-chart" data-toggle="tab">Area</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#sales-chart" data-toggle="tab">Donut</a>
+                </li>
+              </ul>
+            </div>
+          </div><!-- /.card-header -->
+          <div class="card-body">
+            <div class="tab-content p-0">
+              <!-- Morris chart - Sales -->
+              <div class="chart tab-pane active" id="revenue-chart"
+                   style="position: relative; height: 300px;">
+                  <canvas id="revenue-chart-canvas" height="300" style="height: 300px;"></canvas>
+               </div>
+              <div class="chart tab-pane" id="sales-chart" style="position: relative; height: 300px;">
+                <canvas id="sales-chart-canvas" height="300" style="height: 300px;"></canvas>
+              </div>
+            </div>
+          </div><!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+
+
+        <!-- TO DO List -->
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">
+              <i class="ion ion-clipboard mr-1"></i>
+              To Do List
+            </h3>
+
+            <div class="card-tools">
+              <ul class="pagination pagination-sm">
+                <li class="page-item"><a href="#" class="page-link">&laquo;</a></li>
+                <li class="page-item"><a href="#" class="page-link">1</a></li>
+                <li class="page-item"><a href="#" class="page-link">2</a></li>
+                <li class="page-item"><a href="#" class="page-link">3</a></li>
+                <li class="page-item"><a href="#" class="page-link">&raquo;</a></li>
+              </ul>
+            </div>
+          </div>
+          <!-- /.card-header -->
+          <div class="card-body">
+            <ul class="todo-list" data-widget="todo-list">
+              <li>
+                <!-- drag handle -->
+                <span class="handle">
+                  <i class="fas fa-ellipsis-v"></i>
+                  <i class="fas fa-ellipsis-v"></i>
+                </span>
+                <!-- checkbox -->
+                <div  class="icheck-primary d-inline ml-2">
+                  <input type="checkbox" value="" name="todo1" id="todoCheck1">
+                  <label for="todoCheck1"></label>
+                </div>
+                <!-- todo text -->
+                <span class="text">Design a nice theme</span>
+                <!-- Emphasis label -->
+                <small class="badge badge-danger"><i class="far fa-clock"></i> 2 mins</small>
+                <!-- General tools such as edit or delete-->
+                <div class="tools">
+                  <i class="fas fa-edit"></i>
+                  <i class="fas fa-trash-o"></i>
+                </div>
+              </li>
+              <li>
+                <span class="handle">
+                  <i class="fas fa-ellipsis-v"></i>
+                  <i class="fas fa-ellipsis-v"></i>
+                </span>
+                <div  class="icheck-primary d-inline ml-2">
+                  <input type="checkbox" value="" name="todo2" id="todoCheck2" checked>
+                  <label for="todoCheck2"></label>
+                </div>
+                <span class="text">Make the theme responsive</span>
+                <small class="badge badge-info"><i class="far fa-clock"></i> 4 hours</small>
+                <div class="tools">
+                  <i class="fas fa-edit"></i>
+                  <i class="fas fa-trash-o"></i>
+                </div>
+              </li>
+              <li>
+                <span class="handle">
+                  <i class="fas fa-ellipsis-v"></i>
+                  <i class="fas fa-ellipsis-v"></i>
+                </span>
+                <div  class="icheck-primary d-inline ml-2">
+                  <input type="checkbox" value="" name="todo3" id="todoCheck3">
+                  <label for="todoCheck3"></label>
+                </div>
+                <span class="text">Let theme shine like a star</span>
+                <small class="badge badge-warning"><i class="far fa-clock"></i> 1 day</small>
+                <div class="tools">
+                  <i class="fas fa-edit"></i>
+                  <i class="fas fa-trash-o"></i>
+                </div>
+              </li>
+              <li>
+                <span class="handle">
+                  <i class="fas fa-ellipsis-v"></i>
+                  <i class="fas fa-ellipsis-v"></i>
+                </span>
+                <div  class="icheck-primary d-inline ml-2">
+                  <input type="checkbox" value="" name="todo4" id="todoCheck4">
+                  <label for="todoCheck4"></label>
+                </div>
+                <span class="text">Let theme shine like a star</span>
+                <small class="badge badge-success"><i class="far fa-clock"></i> 3 days</small>
+                <div class="tools">
+                  <i class="fas fa-edit"></i>
+                  <i class="fas fa-trash-o"></i>
+                </div>
+              </li>
+              <li>
+                <span class="handle">
+                  <i class="fas fa-ellipsis-v"></i>
+                  <i class="fas fa-ellipsis-v"></i>
+                </span>
+                <div  class="icheck-primary d-inline ml-2">
+                  <input type="checkbox" value="" name="todo5" id="todoCheck5">
+                  <label for="todoCheck5"></label>
+                </div>
+                <span class="text">Check your messages and notifications</span>
+                <small class="badge badge-primary"><i class="far fa-clock"></i> 1 week</small>
+                <div class="tools">
+                  <i class="fas fa-edit"></i>
+                  <i class="fas fa-trash-o"></i>
+                </div>
+              </li>
+              <li>
+                <span class="handle">
+                  <i class="fas fa-ellipsis-v"></i>
+                  <i class="fas fa-ellipsis-v"></i>
+                </span>
+                <div  class="icheck-primary d-inline ml-2">
+                  <input type="checkbox" value="" name="todo6" id="todoCheck6">
+                  <label for="todoCheck6"></label>
+                </div>
+                <span class="text">Let theme shine like a star</span>
+                <small class="badge badge-secondary"><i class="far fa-clock"></i> 1 month</small>
+                <div class="tools">
+                  <i class="fas fa-edit"></i>
+                  <i class="fas fa-trash-o"></i>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <!-- /.card-body -->
+          <div class="card-footer clearfix">
+            <button type="button" class="btn btn-primary float-right"><i class="fas fa-plus"></i> Add item</button>
+          </div>
+        </div>
+        <!-- /.card -->
+      </section>
+      <!-- /.Left col -->
+      <!-- right col (We are only adding the ID to make the widgets sortable)-->
+      <section class="col-lg-5 connectedSortable">
+
+        <!-- Map card -->
+        <div class="card bg-gradient-primary">
+          <div class="card-header border-0">
+            <h3 class="card-title">
+              <i class="fas fa-map-marker-alt mr-1"></i>
+              Visitors
+            </h3>
+            <!-- card tools -->
+            <div class="card-tools">
+              <button type="button" class="btn btn-primary btn-sm daterange" title="Date range">
+                <i class="far fa-calendar-alt"></i>
+              </button>
+              <button type="button" class="btn btn-primary btn-sm" data-card-widget="collapse" title="Collapse">
+                <i class="fas fa-minus"></i>
+              </button>
+            </div>
+            <!-- /.card-tools -->
+          </div>
+          <div class="card-body">
+            <div id="world-map" style="height: 250px; width: 100%;"></div>
+          </div>
+          <!-- /.card-body-->
+          <div class="card-footer bg-transparent">
+            <div class="row">
+              <div class="col-4 text-center">
+                <div id="sparkline-1"></div>
+                <div class="text-white">Visitors</div>
+              </div>
+              <!-- ./col -->
+              <div class="col-4 text-center">
+                <div id="sparkline-2"></div>
+                <div class="text-white">Online</div>
+              </div>
+              <!-- ./col -->
+              <div class="col-4 text-center">
+                <div id="sparkline-3"></div>
+                <div class="text-white">Sales</div>
+              </div>
+              <!-- ./col -->
+            </div>
+            <!-- /.row -->
+          </div>
+        </div>
+        <!-- /.card -->
+
+        <!-- solid sales graph -->
+        <div class="card bg-gradient-info">
+          <div class="card-header border-0">
+            <h3 class="card-title">
+              <i class="fas fa-th mr-1"></i>
+              Sales Graph
+            </h3>
+
+            <div class="card-tools">
+              <button type="button" class="btn bg-info btn-sm" data-card-widget="collapse">
+                <i class="fas fa-minus"></i>
+              </button>
+              <button type="button" class="btn bg-info btn-sm" data-card-widget="remove">
+                <i class="fas fa-times"></i>
+              </button>
+            </div>
+          </div>
+          <div class="card-body">
+            <canvas class="chart" id="line-chart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+          </div>
+          <!-- /.card-body -->
+          <div class="card-footer bg-transparent">
+            <div class="row">
+              <div class="col-4 text-center">
+                <input type="text" class="knob" data-readonly="true" value="20" data-width="60" data-height="60"
+                       data-fgColor="#39CCCC">
+
+                <div class="text-white">Mail-Orders</div>
+              </div>
+              <!-- ./col -->
+              <div class="col-4 text-center">
+                <input type="text" class="knob" data-readonly="true" value="50" data-width="60" data-height="60"
+                       data-fgColor="#39CCCC">
+
+                <div class="text-white">Online</div>
+              </div>
+              <!-- ./col -->
+              <div class="col-4 text-center">
+                <input type="text" class="knob" data-readonly="true" value="30" data-width="60" data-height="60"
+                       data-fgColor="#39CCCC">
+
+                <div class="text-white">In-Store</div>
+              </div>
+              <!-- ./col -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.card-footer -->
+        </div>
+        <!-- /.card -->
+
+      </section>
+      <!-- right col -->
     </div>
-  </div>
+    <!-- /.row (main row) -->
+  </div><!-- /.container-fluid -->
+</section>
+
 @endsection
 
 @push('scripts')
-<script src="{{asset('admin-let/plugins/bootstrap-switch/js/bootstrap-switch.min.js')}}"></script>
-  <script>
-          $("input[data-bootstrap-switch]").each(function(){
-          $(this).bootstrapSwitch('state', $(this).prop('checked'));
-    })
-  </script>
+<script src="{{asset('admin-let/plugins/jquery/jquery.min.js')}}"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="{{asset('admin-let/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<script src="{{asset('admin-let/plugins/sparklines/sparkline.js')}}"></script>
+<script src="{{asset('admin-let/plugins/jqvmap/jquery.vmap.min.js')}}"></script>
+<script src="{{asset('admin-let/plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script>
+
+<script src="{{asset('admin-let/plugins/moment/moment.min.js')}}"></script>
+<script src="{{asset('admin-let/plugins/daterangepicker/daterangepicker.js')}}"></script>
+<script src="{{asset('admin-let/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
+<script src="{{asset('admin-let/plugins/summernote/summernote-bs4.min.js')}}"></script>
+<script src="{{asset('admin-let/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+
+<script src="{{asset('admin-let/plugins/chart.js/Chart.min.js')}}"></script>
+<script src="{{asset('admin-let/plugins/jquery-knob/jquery.knob.min.js')}}"></script>
+<script src="{{asset('admin-let/dist/js/pages/dashboard.js')}}"></script>
+
+
+
 @endpush
